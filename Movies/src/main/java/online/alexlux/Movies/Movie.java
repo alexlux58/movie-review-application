ccf7e757c6fd8e636a7850178f120ad774d95567
Alex Lux <alexlux@live.com>
2023-08-17 11:23:33 -0700

    create Mongo DB/Repository and controller for Movie

---
+++ Movies/src/main/java/online/alexlux/Movies/Movie.java
@@ -0,0 +1,28 @@
+package online.alexlux.Movies;
+
+import java.util.List;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Document(collection = "movies")
+@Data
+@AllArgsConstructor
+public class Movie {
+    @Id
+    private ObjectId id;
+    private String imdbId;
+    private String title;
+    private String releaseDate;
+    private String trailerLink;
+    private List<String> genres;
+    private String poster;
+    private List<String> backdrops;
+    @DBRef
+    private List<Review> reviewIds;
+}
---
+++ Movies/src/main/java/online/alexlux/Movies/Review.java
@@ -0,0 +1,18 @@
+package online.alexlux.Movies;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Document(collection = "reviews")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Review {
+    @Id
+    private Object id;
+    private String body;
+}
